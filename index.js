6f3d68508ee93e4f3e3ce9dca9e0f4ad514e67fa
Jesse SMith <jesse.lee.smith@gmail.com>
2017-12-15 02:14:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function add(num1, num2){
   return num1 + num2;
 }
+
+function subtract(num1, num2){
+  return num1 - num2;
+}
+
+function multiply(num1, num2){
+  return num1 * num2;
+}
+
+function divide()
